df16f8504380256ad7905e2e419f56f947bd5fea
Eduardo Lopes <edulopestj@gmail.com>
2017-06-23 23:55:10 -0300

    working props in react

--- ex/index.jsx
+++ ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDom  from 'react-dom'
 import Component from './component'
 
-ReactDom.render(<Component />, document.getElementById('app'));
+ReactDom.render(<Component value="Eduardo Lopes"/>, document.getElementById('app'));
